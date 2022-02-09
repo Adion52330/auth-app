9c998390226a7100ed786d8e184038008e19475e
Aditya <85357798+Adion52330@users.noreply.github.com>
2022-02-09 16:03:32 +0530

    Update Biodata.tsx

--- components/Biodata.tsx
+++ components/Biodata.tsx
@@ -8,14 +8,14 @@ const Biodata = () => {
   const [email, setEmail] = useState(session?.user?.email)
   const [image, setImage] = useState(session?.user?.image)
   return (
-    <div className="max-w-screen">
+    <div className="max-w-screen min-h-screen">
       <Header />
       <div className="my-4 text-center">
         <h1 className="text-4xl font-semibold">Personal Info</h1>
         <p className="my-4">Basic Info like, your image and name</p>
       </div>
-      <div className="my-10 flex w-full flex-col items-center justify-center rounded-lg border-2 border-[#E0E0E0] md:mx-auto md:max-w-6xl">
-        <div className="flex max-w-screen items-center justify-between border-b-2 border-[#E0E0E0] px-10 py-4">
+      <div className="my-10 flex max-w-screen flex-col items-center justify-center rounded-lg border-2 border-[#E0E0E0] md:mx-auto md:max-w-6xl">
+        <div className="flex w-full items-center justify-between border-b-2 border-[#E0E0E0] px-10 py-4">
           <div>
             <h1 className="text-3xl">Profile</h1>
             <p className="text-[#828282]">
@@ -26,15 +26,15 @@ const Biodata = () => {
             Edit
           </div>
         </div>
-        <div className="flex w-full items-center space-x-40 border-b-2 border-[#E0E0E0] px-10 py-4">
+        <div className="flex w-full items-center md:space-x-40 space-x-10 border-b-2 border-[#E0E0E0] px-10 py-4">
           <p className="text-[#bdbdbd]">Photo</p>
           <img src={image!} className="h-24 w-24 rounded-full" alt="" />
         </div>
-        <div className="flex h-[73.26px] w-full items-center space-x-40 border-b-2 border-[#E0E0E0] px-10 py-4">
+        <div className="flex h-[73.26px] w-full items-center md:space-x-40 space-x-10 border-b-2 border-[#E0E0E0] px-10 py-4">
           <p className="text-[#bdbdbd]">Name</p>
           <p className="font-semibold text-[#333333]">{name}</p>
         </div>
-        <div className="flex h-[73.26px] w-full items-center space-x-40 border-b-2 border-[#E0E0E0] px-10 py-4">
+        <div className="flex h-[73.26px] w-full items-center md:space-x-40 space-x-10 border-b-2 border-[#E0E0E0] px-10 py-4">
           <p className="text-[#bdbdbd]">Email</p>
           <p className="font-semibold text-[#333333]">{email}</p>
         </div>
